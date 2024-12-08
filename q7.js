78bc818338291469dd389b23eb088445b5fe9424
eduarda-sofferreira <espf1@aluno.ifal.edu.br>
2024-12-08 14:10:44 -0300

    Create q7.js

---
+++ q7.js
@@ -0,0 +1,9 @@
+var c = 1
+var soma = 0
+while ( c <= 20) {
+    var idade = parseInt (prompt ("informe sua idade"))
+    soma += idade
+    c++
+}
+var media = soma / 20
+    console.log("A média das idades fornecidas é" + media)
